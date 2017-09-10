004ccf8408b906de8e57f80b9d7116428a010c58
BlytheS2564 <31484989+BlytheS2564@users.noreply.github.com>
2017-09-10 16:46:04 -0400

    Add files via upload
    Asks for total sales then gives 23% profit annual profit.

---
+++ M2T1_SalesPrediction_BlytheS.py
@@ -0,0 +1,18 @@
+#CTI-110
+#M2T1 - Sales Prediction
+#Samuel Blythe
+#9/6/17
+#
+
+#Greeting
+print("Hello")
+
+#Obtain projected total sales
+totalSales = float(input('What are your projected sales?'))
+
+#Calculate profit
+annualProfit = totalSales*.23
+
+#Display profit
+print('Your profit is $', format(annualProfit, ',.2f'))
+
